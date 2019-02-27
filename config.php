48bd7cb432de7cc4aa87fb5a369ec53645c4d926
deepcell <deepcell@gmail.com>
2019-02-27 14:27:38 +0700

    ..

--- config.php
+++ config.php
@@ -1,8 +1,6 @@
 <?php
-error_reporting(0);
-
 // para faciliar nossas vidas vamos usar medoo para conexao com o banco.
-include_once 'medoo.php';
+include_once 'class/medoo.php';
 
 // PDO via medoo (File Encode: ISO 8859 - 1 (Latin - 1) \ banco collation Latin1)
 $database = new medoo(array(
